89346c8c89dd41b69b7d668f86506b0106caafdd
Eric Chin <eric.chin@liferay.com>
2017-11-27 11:20:23 -0800

    Add portlet keys

---
+++ modules/data-entry-portlet/data-entry-portlet-api/src/main/java/foundation/fitness/portal/service/constants/ReportingPortletKeys.java
@@ -0,0 +1,17 @@
+package foundation.fitness.portal.service.constants;
+
+/**
+ * @author Eric Chin
+ */
+public class ReportingPortletKeys {
+
+	public static final String DISTRICT_REPORTING_PORTLET =
+		"foundation_fitness_portal_reporting_district_DistrictReportingPortlet";
+
+	public static final String SCHOOL_REPORTING_PORTLET =
+		"foundation_fitness_portal_reporting_district_SchoolReportingPortlet";
+
+	public static final String STUDENT_REPORTING_PORTLET =
+		"foundation_fitness_portal_reporting_district_StudentReportingPortlet";
+
+}
