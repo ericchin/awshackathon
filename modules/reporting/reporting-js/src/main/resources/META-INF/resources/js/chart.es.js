13aeda13d442843135bcaea7e025052d668da1d7
Eric Chin <eric.chin@liferay.com>
2017-11-27 11:32:43 -0800

    Add highcharts js module

---
+++ modules/reporting/reporting-js/src/main/resources/META-INF/resources/js/chart.es.js
@@ -0,0 +1,32 @@
+import Highcharts3d from "./utils/highcharts-3d.es";
+import Highcharts from "highcharts/highcharts.src";
+
+let default3dEdgeColor = {
+	borderColor: 'transparent',
+	edgeColor: 'transparent'
+}
+
+Highcharts3d(Highcharts);
+
+// API for Highcharts can be found at: http://api.highcharts.com/highcharts
+
+class Chart {
+	constructor(config) {
+		this.config = config;
+	}
+
+	renderChart(container) {
+		this.config.chart.renderTo = container;
+
+		if (this.config.chart.options3d) {
+			this.config = Object.assign(this.config, default3dEdgeColor);
+		}
+
+		let chart = new Highcharts.Chart(
+			container,
+			this.config
+		);
+	}
+}
+
+export default Chart;
